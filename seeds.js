8199c4735c1329fffd6021022418610e2720164e
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-06-21 16:52:49 -0300

    Styling Show

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's rest", 
         image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_1280.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id cursus tortor. Nam elit ligula, auctor et turpis at, dapibus fringilla odio. In bibendum, libero convallis viverra aliquet, est metus consectetur justo, vitae efficitur ante enim sit amet augue. In felis dui, ultricies at mollis eget, vestibulum aliquet purus. Proin mattis nunc lorem, sed tincidunt tellus faucibus a. Pellentesque sed ex a velit dictum mattis ut quis nisi."
     },
     {
         name: "Granite Hill", 
         image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id cursus tortor. Nam elit ligula, auctor et turpis at, dapibus fringilla odio. In bibendum, libero convallis viverra aliquet, est metus consectetur justo, vitae efficitur ante enim sit amet augue. In felis dui, ultricies at mollis eget, vestibulum aliquet purus. Proin mattis nunc lorem, sed tincidunt tellus faucibus a. Pellentesque sed ex a velit dictum mattis ut quis nisi."
     },
     {
         name: "Mountain Goat's Rest", 
         image: "https://cdn.pixabay.com/photo/2015/03/26/10/29/camping-691424_1280.jpg",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id cursus tortor. Nam elit ligula, auctor et turpis at, dapibus fringilla odio. In bibendum, libero convallis viverra aliquet, est metus consectetur justo, vitae efficitur ante enim sit amet augue. In felis dui, ultricies at mollis eget, vestibulum aliquet purus. Proin mattis nunc lorem, sed tincidunt tellus faucibus a. Pellentesque sed ex a velit dictum mattis ut quis nisi."
     }
 ]
 
