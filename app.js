0dd67b47abb2bb8c31c1e1324857c9a61a4c091d
Paulo Varassin <pauloarthurvm@hotmail.com>
2020-07-01 18:52:05 -0300

    Changed listen

--- app.js
+++ app.js
@@ -55,7 +55,11 @@ app.use(indexRoutes)
 app.use("/campgrounds", campgroundRoutes)
 app.use("/campgrounds/:id/comments", commentRoutes)
 
-app.listen(3000, function () {
+// app.listen(3000, function () {
+//     console.log("YelpCamp Server is up!")
+//     console.log("")
+// })
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("YelpCamp Server is up!")
     console.log("")
 })
